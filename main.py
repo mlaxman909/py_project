3f281294dd4983c70f504eee6e62a6e6f0503506
mlaxman909 <m.laxman999999@gmail.com>
2026-09-18 14:20:20 +0530

    Add school management system menu

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Press 1 to Register student.")
+print("Press 2 to Register teacher.")
+print("Press 3 to add marks of student.")
+print("Press 4 for student details.")
+print("Press 5 for teacher details.")
+
+
+user = int(input("Please Enter your choice(1 to 5):-  "))
+
+
+
+
+if user == 1:
+    pass
+elif user == 2:
+    pass
+elif user == 3:
+    pass
+elif user == 4:
+    pass
+elif user == 5:
+    pass
+else:
+    print("Please enter your choice between 1 to 5 :-")
